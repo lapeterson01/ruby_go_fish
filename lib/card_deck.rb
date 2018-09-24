8ed0cd3fbd51485f4a46a91396277cd28aff4831
Logan Peterson <lapeterson01@gmail.com>
2018-09-24 17:58:33 -0400

    added html and css for home page

--- lib/card_deck.rb
+++ lib/card_deck.rb
@@ -13,22 +13,45 @@ class CardDeck
   end
 
   def shuffle!
-    @cards.shuffle!
+    cards.shuffle!
   end
 
   def deal
-    @cards.shift
+    cards.shift
   end
 
   def out_of_cards?
-    @cards.empty?
+    cards.empty?
   end
 
   def ==(other)
     equal = true
     other.cards.each do |card2|
-      equal = false if @cards[other.cards.index(card2)] != card2
+      equal = false if cards[other.cards.index(card2)] != card2
     end
     equal
   end
+end
+
+def play
+  output = ''
+  output = @socket.gets until output.include? 'start'
+  @socket.puts 'start'
+  next_request = ''
+  until output.include? 'Winner'
+    if output.include? 'hand'
+      RANKS.each do |rank|
+        if output.include? rank
+          next_request = rank
+          break
+        end
+      end
+      @socket.gets
+    elsif output.include? 'Type'
+      @socket.puts "Player 2 for #{next_request}"
+    else
+      @socket.gets
+    end
+  end
+  @socket.gets
 end
--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 # creates and runs a game of go fish
 class Game
-  attr_reader :deck, :players, :turn, :started
+  attr_reader :deck, :players, :turn, :started, :books_display_name, :books_display_number, :string_to_display, :winning_books
 
   def initialize(number_of_players, deck = CardDeck.new)
     @deck = deck
@@ -15,8 +15,8 @@ class Game
   end
 
   def start
-    @deck.shuffle!
-    @players.each_value { |player| @players.length < 4 ? deal_cards(7, player) : deal_cards(5, player) }
+    deck.shuffle!
+    players.each_value { |player| @players.length < 4 ? deal_cards(7, player) : deal_cards(5, player) }
     @started = true
   end
 
@@ -25,16 +25,16 @@ class Game
 
     return 'You cannot ask yourself for a card' if player_name == @turn.name
 
-    player = @players[player_name]
+    player = players[player_name]
     get_catch = player.hand[rank] ? handle_asked_player_has_card(player, rank) : handle_go_fish(rank)
     handle_player_got_book
     next_player_turn unless get_catch
-    @books_display_number > 0 ? @string_to_display.concat(".. #{@books_display_name} got #{@books_display_number} #{book_or_books}") : @string_to_display
+    books_display_number > 0 ? string_to_display.concat(".. #{books_display_name} got #{books_display_number} #{book_or_books}") : string_to_display
   end
 
   def winner
     @winning_books = 0
-    @players.each_value { |player| calculate_winner(player) } if @deck.out_of_cards? || players_out_of_cards
+    players.each_value { |player| calculate_winner(player) } if deck.out_of_cards? || players_out_of_cards
     @winner
   end
 
@@ -42,7 +42,7 @@ class Game
 
   def deal_cards(int, player)
     int.times do
-      card = @deck.deal
+      card = deck.deal
       player.retrieve_card(card)
     end
   end
@@ -56,43 +56,43 @@ class Game
 
   def handle_go_fish(rank)
     card = deck.deal
-    @turn.retrieve_card(card)
+    turn.retrieve_card(card)
     @string_to_display = "#{@turn.name} asked for #{rank} and drew #{card.rank} of #{card.suit} from pool"
     _get_catch = card.rank == rank
   end
 
   def handle_player_got_book
-    @books_display_name, @books_display_number = @turn.name, 0
-    @turn.hand.each_key do |set|
-      next unless @turn.hand[set].length == 4
+    @books_display_name, @books_display_number = turn.name, 0
+    turn.hand.each_key do |set|
+      next unless turn.hand[set].length == 4
 
-      @turn.books += 1
-      @turn.give_up_cards(set)
+      turn.books += 1
+      turn.give_up_cards(set)
       @books_display_number += 1
     end
   end
 
   def next_player_turn
-    player_names = @players.keys
-    @turn = if @players.key(@turn) == player_names.last
-              @players[player_names[0]]
+    player_names = players.keys
+    @turn = if players.key(turn) == player_names.last
+              players[player_names[0]]
             else
-              @players[player_names[player_names.index(@players.key(@turn)) + 1]]
+              @players[player_names[player_names.index(@players.key(turn)) + 1]]
             end
   end
 
   def players_out_of_cards
     any_empty_hands = false
-    @players.each_value do |player|
+    players.each_value do |player|
       any_empty_hands = true if player.out_of_cards?
     end
     any_empty_hands
   end
 
   def calculate_winner(player)
-    if player.books > @winning_books
+    if player.books > winning_books
       @winner, @winning_books = player, player.books
-    elsif player.books == @winning_books && player.books > 0
+    elsif player.books == winning_books && player.books > 0
       @winner.class == Array ? @winner.push(player) : @winner = [@winner, player]
     end
   end
@@ -101,10 +101,10 @@ class Game
     cards_strings = []
     cards.each { |card| cards_strings.push("#{card.rank} of #{card.suit}") }
     cards_display = cards_strings.join(', ')
-    @string_to_display = ["#{@turn.name} took ", " from #{player.name}"].join(cards_display)
+    @string_to_display = ["#{turn.name} took ", " from #{player.name}"].join(cards_display)
   end
 
   def book_or_books
-    @books_display_number == 1 ? 'book' : 'books'
+    books_display_number == 1 ? 'book' : 'books'
   end
 end
